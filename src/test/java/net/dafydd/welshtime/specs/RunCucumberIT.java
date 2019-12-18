56c9e99d4b07d921bd046548084ec015215eeb9d
daf <daf@dafydd.net>
2019-12-18 11:48:54 +0000

    add integration-test cucumber features (not connected up to code/assertions)

---
+++ src/test/java/net/dafydd/welshtime/specs/RunCucumberIT.java
@@ -0,0 +1,12 @@
+package net.dafydd.welshtime.specs;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "json:target/jsonreports.json", "html:target/htmlreports" },
+        glue = "net/dafydd/welshtime/specs")
+public class RunCucumberIT {
+}
---
+++ src/test/resources/net/dafydd/welshtime/specs/welshtime.feature
@@ -0,0 +1,9 @@
+Feature: Check the time in words
+  Some explanatory blurb goes here...
+
+  Scenario: Simple example
+    Given the time "09:00"
+    When translated
+    Then it maps to time "naw o'r gloch"
+
+
